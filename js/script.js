4604c635769a83d1a58d4ef8942775b12043e6e0
Lukasz Zak <thenaturat88@gmail.com>
2021-12-01 22:28:04 +0100

    function added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-console.log("Cześć");
-
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-themeName");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body__light");
-
-    if (body.classList.contains("body__light")) {
-        themeName.innerText = "Włącz ciemne tło";
-    } else {
-        themeName.innerText = "Włącz jasne tło";
+{
+    const welcome = () => {
+        console.log("Cześć");
     }
-});
+
+    const OnChangeBackgroundClick = () => {
+        let body = document.querySelector(".body");
+        let themeName = document.querySelector(".js-themeName");
+        body.classList.toggle("body__light");
+
+        if (body.classList.contains("body__light")) {
+            themeName.innerText = "Włącz ciemne tło";
+        } else {
+            themeName.innerText = "Włącz jasne tło";
+        }
+    }
+    const init = () => {
+        let button = document.querySelector(".js-button");
+       
+
+        button.addEventListener("click", OnChangeBackgroundClick);
+
+        welcome();
+};
+  
+init();
+
+}
